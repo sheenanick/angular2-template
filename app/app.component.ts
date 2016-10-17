d640ff6803061671f517be5d64fc0c2a4d521493
Sheena Nickerson <sheenanick@gmail.com>
2016-10-16 21:15:33 -0700

    add bootstrap to bower dependencies, and container to html

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,7 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
-  <h1>My First Angular 2 App</h1>
+  <div class="container">
+    <h1 class="text-center">My First Angular 2 App</h1>
+  </div>
   `
 })
 
